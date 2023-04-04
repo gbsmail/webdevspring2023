c045ed0879d79f8577563792ef99aaf983d7f5a4
gbsmail <117954989+gbsmail@users.noreply.github.com>
2023-04-04 19:23:33 -0400

    editcode

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 90, left: 40},
+var margin = {top: 10, right: 30, bottom: 40, left: 100},
     width = 460 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("my_dataviz")
+var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -13,36 +13,37 @@ var svg = d3.select("my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Parse the Data
-d3.csv("https://raw.githubusercontent.com/gbsmail/webdevspring2023/main/asamchartdata.csv?token=GHSAT0AAAAAAB6D6KI2QF2YZ6524D47QSRGZBMVT3A", function(data) {
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
 
-// X axis
-var x = d3.scaleBand()
-  .range([ 0, width ])
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 13000])
+    .range([ 0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+// Y axis
+var y = d3.scaleBand()
+  .range([ 0, height ])
   .domain(data.map(function(d) { return d.Service; }))
   .padding(1);
 svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+  .call(d3.axisLeft(y))
 
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, 13000])
-  .range([ height, 0]);
-svg.append("g")
-  .call(d3.axisLeft(y));
 
 // Lines
 svg.selectAll("myline")
   .data(data)
   .enter()
   .append("line")
-    .attr("x1", function(d) { return x(d.Service); })
-    .attr("x2", function(d) { return x(d.Service); })
-    .attr("y1", function(d) { return y(d.Value); })
-    .attr("y2", y(0))
+    .attr("x1", function(d) { return x(d.Value); })
+    .attr("x2", x(0))
+    .attr("y1", function(d) { return y(d.Service); })
+    .attr("y2", function(d) { return y(d.Service); })
     .attr("stroke", "grey")
 
 // Circles
@@ -50,8 +51,8 @@ svg.selectAll("mycircle")
   .data(data)
   .enter()
   .append("circle")
-    .attr("cx", function(d) { return x(d.Service); })
-    .attr("cy", function(d) { return y(d.Value); })
+    .attr("cx", function(d) { return x(d.Value); })
+    .attr("cy", function(d) { return y(d.Service); })
     .attr("r", "4")
     .style("fill", "#69b3a2")
     .attr("stroke", "black")
