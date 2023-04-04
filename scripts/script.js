14017e6e2f9cfa2c70c6bfb8692a708660510832
gbsmail <117954989+gbsmail@users.noreply.github.com>
2023-04-04 19:17:20 -0400

    addedrightscript

--- scripts/script.js
+++ scripts/script.js
@@ -1,70 +1,58 @@
-const data = [
-  { category: 'SNAP Benefits', white: 7, asian: 14 },
-  { category: 'Community Anti-Poverty Participants', white: 0.0671, asian: 0.0984 },
-  { category: "Veteran's Benefits", white: 0.000860516, asian: 0.008399965 },
-  { category: 'Older Adult Center Meal Participants', white: 1, asian: 6 },
-  { category: 'Section 8 Housing', white: 0.010919654, asian: 0.004990078 },
-  { category: 'Homeless Shelter Use in 2021', white: 0.008308433, asian: 0.007485117 },
-];
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 90, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-const container = d3.select("#plot");
+// append the svg object to the body of the page
+var svg = d3.select("my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-const width = 600;
-const height = 400;
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/gbsmail/webdevspring2023/main/asamchartdata.csv?token=GHSAT0AAAAAAB6D6KI2QF2YZ6524D47QSRGZBMVT3A", function(data) {
 
-const svg = container.append("svg")
-  .attr("width", width)
-  .attr("height", height);
+// X axis
+var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(data.map(function(d) { return d.Service; }))
+  .padding(1);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
 
-const xScale = d3.scaleBand()
-  .domain(data.map(d => d.category))
-  .range([0, width])
-  .padding(0.1);
+// Add Y axis
+var y = d3.scaleLinear()
+  .domain([0, 13000])
+  .range([ height, 0]);
+svg.append("g")
+  .call(d3.axisLeft(y));
 
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(data, d => Math.max(d.white, d.asian))])
-  .range([height, 0]);
-
-const groups = svg.selectAll("g")
+// Lines
+svg.selectAll("myline")
   .data(data)
   .enter()
-  .append("g")
-  .attr("transform", d => `translate(${xScale(d.category)}, 0)`);
+  .append("line")
+    .attr("x1", function(d) { return x(d.Service); })
+    .attr("x2", function(d) { return x(d.Service); })
+    .attr("y1", function(d) { return y(d.Value); })
+    .attr("y2", y(0))
+    .attr("stroke", "grey")
 
-groups.append("circle")
-  .attr("cx", xScale.bandwidth() / 2)
-  .attr("cy", d => yScale(d.white))
-  .attr("r", 5)
-  .attr("fill", "white")
-  .attr("stroke", "black");
-
-groups.append("circle")
-  .attr("cx", xScale.bandwidth() / 2)
-  .attr("cy", d => yScale(d.asian))
-  .attr("r", 5)
-  .attr("fill", "black")
-  .attr("stroke", "black");
-
-const xAxis = d3.axisBottom(xScale);
-
-const yAxis = d3.axisLeft(yScale);
-
-svg.append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(xAxis);
-
-svg.append("g")
-  .call(yAxis);
-
-svg.append("text")
-  .attr("x", width / 2)
-  .attr("y", height + 40)
-  .attr("text-anchor", "middle")
-  .text("Category");
-
-svg.append("text")
-  .attr("x", -height / 2)
-  .attr("y", -50)
-  .attr("text-anchor", "middle")
-  .attr("transform", "rotate(-90)")
-  .text("Value");
+// Circles
+svg.selectAll("mycircle")
+  .data(data)
+  .enter()
+  .append("circle")
+    .attr("cx", function(d) { return x(d.Service); })
+    .attr("cy", function(d) { return y(d.Value); })
+    .attr("r", "4")
+    .style("fill", "#69b3a2")
+    .attr("stroke", "black")
+})
